2708f51e9a18beed0c3d5ce8bb1356a6a1de33d9
bravysandhu <bravysandhu@gmail.com>
2016-11-13 18:47:22 +0530

    timer

--- collection.js
+++ collection.js
@@ -1,1 +1,13 @@
 Tasks = new Mongo.Collection('tasks');
+
+Tasks.allow({
+  insert() {return true;},
+  update() {return true;},
+  remove() {return true;}
+});
+
+Tasks.deny({
+  insert() {return false;},
+  update() {return false;},
+  remove() {return false;}
+});
